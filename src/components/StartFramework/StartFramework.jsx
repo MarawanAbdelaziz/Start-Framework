fd8e4438c1bf2d0d5733acb657528549511d83e3
MarawanAbdelaziz <marawan.abdelaziz33@gmail.com>
2024-02-06 20:41:28 +0200

    img1

--- src/components/StartFramework/StartFramework.jsx
+++ src/components/StartFramework/StartFramework.jsx
@@ -1,15 +1,15 @@
 import { Helmet } from "react-helmet";
 import { FaStar } from "react-icons/fa";
-
+import img1 from "../../assets/avataaars.svg";
 const StartFramework = () => {
   return (
     <>
       <div className="w-[400px] flex flex-col justify-center items-center h-screen mx-auto">
-      <Helmet>
+        <Helmet>
           <meta charSet="utf-8" />
           <title>Start Framework</title>
         </Helmet>
-        <img className="w-[70%]" src="src/assets/avataaars.svg" alt="" />
+        <img className="w-[70%]" src={`${img1}`} alt="" />
         <h2 className="text-[40px] font-bold mt-5">START FRAMEWORK</h2>
         <div className="flex items-center mt-2 mb-4">
           <div className="w-24 me-3 h-1 bg-white"></div>
